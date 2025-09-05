b303518d8cf83522624b4550d2ab3ddf4e8f522c
benicandra <benidalucandra@gmail.com>
2025-09-05 14:01:05 +0700

    Perbaikan nama komponen

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -14,7 +14,7 @@ import InputNama from "../components/formComponents/InputNama";
 import InputEmail from "../components/formComponents/InputEmail";
 import InputRegion from "../components/formComponents/InputRegion";
 import InputNoHp from "../components/formComponents/InputNoHp";
-import InputPassword from "../components/formComponents/inputPassword";
+import InputPassword from "./formComponents/InputPassword.jsx";
 import Button from "../components/button/Button";
 
 const Profile = () => {
